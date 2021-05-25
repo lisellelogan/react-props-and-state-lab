34ab37150a697abfdff73d96eda22c450e0dee22
lisellelogan <liselle.logan@yahoo.com>
2021-05-25 13:23:08 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,16 @@
+// App should pass a callback prop, onChangeType, to <Filters />. This callback needs to update <App />'s state.filters.type
+
+// <Filters /> needs a callback prop, onFindPetsClick. When the <Filters /> component calls onFindPetsClick, <App /> should fetch a list of pets using fetch().
+
+// Assuming your app is up and running, you can make a fetch to this exact URL: /api/pets with an optional query parameter to get your data.
+// Use App's state.filters to control/update this parameter
+// If the type is 'all', send a request to /api/pets
+// If the type is 'cat', send a request to /api/pets?type=cat. Do the same thing for dog and micropig.
+// The pet data received will include information on individual pets and their adoption status.
+// Set <App/>'s state.pets with the results of your fetch request so you can pass the pet data down as props to <PetBrowser />
+// Even though we're using fetch here, its responses have been mocked in order to make the tests work properly. That means it's important to use the exact URLs as described above, or your tests will fail!
+// Finally, App should pass a callback prop, onAdoptPet, to <PetBrowser />. This callback should take in an id for a pet, find the matching pet in state.pets and set the isAdopted property to true.
+
 import React from 'react'
 
 import Filters from './Filters'
@@ -15,6 +28,17 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = event => {
+    this.setState({
+      filters: {...this.state.filters,
+      type: event.target.value}
+    })
+  }
+
+  fetchPets = () => {
+    
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,7 +48,9 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters 
+              onChangeType={this.onChangeType}
+              onFindPetsClick={this.fetchPets} />
             </div>
             <div className="twelve wide column">
               <PetBrowser />
